36df6d4d6aa5937f9fd4c493bdaae46fdb909f00
ssebastiancardona <ss3bastiancardona@gmail.com>
2022-07-20 22:06:09 -0500

    4-outer_inner.dart

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,8 +1,8 @@
-void afuera(String nombre, String id) {
+void outer(String nombre, String cc) {
   String inner() {
     var disidente = nombre.split(" ");
     var petu = disidente[1].substring(0, 1) + "." + disidente[0];
-    return "Hello Agent ${petu} your id is $id";
+    return "Hello Agent ${petu} your id is $cc";
   }
 
   print(inner());
